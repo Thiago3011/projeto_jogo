b8226b3aea4e401af0c43e4d124fdb855944a450
Thiago3011 <thiago.silva1001@outlook.com>
2025-02-24 20:32:18 -0300

    criando a mecanica de combate

--- jogo.py
+++ jogo.py
@@ -16,6 +16,17 @@ class Personagem:
     def exibir_detalhes(self):
         return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNível: {self.get_nivel()}"
     
+    def receber_ataque(self, dano):
+        self.__vida -= dano
+        if self.__vida < 0:
+            self.__vida = 0
+            
+    def atacar(self, alvo):
+        dano = self.__nivel * 2
+        alvo.receber_ataque(dano)
+        print(f"\n{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
+        
+    
 class Heroi(Personagem):
     def __init__(self, nome, vida, nivel, habilidade):
         super().__init__(nome, vida, nivel)
@@ -26,6 +37,11 @@ class Heroi(Personagem):
     
     def exibir_detalhes(self):
         return f"{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}\n"
+    
+    def ataque_especial(self, alvo):
+        dano = self.get_nivel() * 5
+        alvo.receber_ataque(dano)
+        print(f"\n{self.get_nome()} usou a habilidade especial {self.get_habilidade()} em {alvo.get_nome()} e causou {dano} de dano!")
         
 class Inimigo(Personagem):
     def __init__(self, nome, vida, nivel, tipo):
@@ -54,6 +70,21 @@ class Jogo:
             input("Pressione 'Enter' para atacar...")
             escolha = input("Escolha (1- Ataque Normal, 2- Ataque Especial): ")
             
+            if escolha == "1":
+                self.heroi.atacar(self.inimigo)
+            elif escolha == "2":
+                self.heroi.ataque_especial(self.inimigo)
+            else:
+                print("\nEscolha inválida. Escolha novamente...")
+                
+            
+            if self.inimigo.get_vida() > 0:
+                self.inimigo.atacar(self.heroi)
+            
+        if self.heroi.get_vida() > 0:
+            print("\nParabéns, você venceu a batalha!")
+        else:
+            print("\nVocê foi derrotado!")   
 # Criar a instância do jogo e iniciar a batalha
 
 jogo = Jogo()
